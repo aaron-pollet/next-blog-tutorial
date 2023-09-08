323d3f98a2f623f0c6e3c92073d3432f346546ab
aaron-pollet <pollet_aaron@hotmail.com>
2023-09-08 15:32:57 +0200

    start footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
 import styles from './footer.module.css';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image
+            src='/logo.png'
+            alt=''
+            width={50}
+            height={50}
+          />
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque alias fugit quos, iure
+          odio eos architecto officiis. Sed, cumque? Sapiente deserunt ex repudiandae reprehenderit
+          aperiam officia obcaecati? Praesentium, odit doloribus.
+        </p>
+        <div className={styles.icons}>
+          <Image
+            src='/instagram.png'
+            alt='instagram'
+            width={20}
+            height={20}
+          />
+          <Image
+            src='/facebook.png'
+            alt='facebook'
+            width={20}
+            height={20}
+          />
+          <Image
+            src='/youtube.png'
+            alt='youtube'
+            width={20}
+            height={20}
+          />
+          <Image
+            src='/tiktok.png'
+            alt='tiktok'
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
+      <div className={styles.links}></div>
+    </div>
+  );
 };
 
 export default Footer;
